afe45b40880fba8c34d14ef2eaf7c95f2891aab8
Saurabh Singhal <singhal.saurabh.mnnit@gmail.com>
2015-12-19 17:09:40 +0530

    Better way

--- El.py
+++ El.py
@@ -1,9 +1,10 @@
 # Given a variable page which contains the contents of a web page( the HTML code )
 # Find the first link
 
-page = "<html><head><title>EL</title></head><body><a href='www.google.com'>Google</a><br/><a href='www.fb.com'>Facebook</a></body></html>"
+page = '<html><head><title>EL</title></head><body><a href="www.google.com">Google</a><br/><a href="www.fb.com">Facebook</a></body></html>'
 linkString = "<a href="
-startLink = page.find( linkString ) + 9
-endLink = page.find( "'>" )
-link = page[ startLink : endLink ]
+startLink = page.find( linkString )
+startQuote = page.find( '"', startLink )
+endQuote = page.find( '"', startQuote + 1 )
+link = page[ startQuote + 1 : endQuote ]
 print link
